dd075cdb38194a201e24727d4ca1b58943fde6c3
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-22 18:52:14 +0900

    perf: ⚡️ ini header border

--- src/components/HeaderRoot/HeaderRoot.tsx
+++ src/components/HeaderRoot/HeaderRoot.tsx
@@ -6,14 +6,16 @@ const ImageLogo = dynamic(() => import("@/components/ImageLogo"), {
 
 export default function HeaderRoot(): JSX.Element {
   return (
-    <div className="container relative z-20 mx-auto">
-      <div className="sm:px-6 px-4">
-        <div className="md:justify-start md:space-x-10 flex items-center justify-between py-6 border-b-2 border-gray-100">
-          <div className="lg:w-0 lg:flex-1">
-            <ImageLogo />
+    <header className="border-b-2 border-gray-100">
+      <div className="container relative z-20 mx-auto">
+        <div className="sm:px-6 px-4">
+          <div className="md:justify-start md:space-x-10 flex items-center justify-between py-6">
+            <div className="lg:w-0 lg:flex-1">
+              <ImageLogo />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
