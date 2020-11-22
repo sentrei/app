b42947ff60e224bde932776a23e4478c33ec395a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-22 15:47:10 +0900

    perf: ⚡️ refactor landing hero

--- src/components/LandingHero/LandingHero.tsx
+++ src/components/LandingHero/LandingHero.tsx
@@ -1,18 +1,26 @@
 export default function LandingHero(): JSX.Element {
   return (
     <section className="max-w-7xl sm:mt-24 px-4 mx-auto mt-16">
-      <div className="text-center">
+      <div className="sm:text-left md:text-center">
         <h1 className="sm:text-5xl md:text-6xl text-4xl font-extrabold tracking-tight text-gray-900">
-          <span className="xl:inline block">Data to enrich your</span>
+          <span className="xl:inline block">
+            The all-in-one serverless backend infrastructure
+          </span>
           <span className="xl:inline block text-indigo-600">
             {" "}
-            online business
+            of your dreams
           </span>
         </h1>
-        <p className="sm:text-lg md:mt-5 md:text-xl md:max-w-3xl max-w-md mx-auto mt-3 text-base text-gray-500">
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
+        <p className="md:mx-auto sm:text-lg md:mt-5 md:text-xl md:max-w-3xl max-w-md mt-3 text-base text-gray-500">
+          Sentrei combines the best{" "}
+          <span className="bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent">
+            developer experience{" "}
+          </span>
+          with blazing fast end-user performance for your entire{" "}
+          <span className="bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 text-transparent">
+            backend infrastrucuture
+          </span>{" "}
+          .
         </p>
         <div className="max-w-lg mx-auto mt-8 text-center">
           <p className="text-base font-medium text-gray-900">
