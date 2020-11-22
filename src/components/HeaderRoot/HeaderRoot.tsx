677d968c3b77882c18889c3f642aa47ae2db35c0
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-22 16:43:29 +0900

    feat: 🎸 ini header root

---
+++ src/components/HeaderRoot/HeaderRoot.tsx
@@ -0,0 +1,19 @@
+import dynamic from "next/dynamic";
+
+const ImageLogo = dynamic(() => import("@/components/ImageLogo"), {
+  ssr: false,
+});
+
+export default function HeaderRoot(): JSX.Element {
+  return (
+    <div className="container relative z-20 mx-auto">
+      <div className="sm:px-6 px-4">
+        <div className="md:justify-start md:space-x-10 flex items-center justify-between py-6 border-b-2 border-gray-100">
+          <div className="lg:w-0 lg:flex-1">
+            <ImageLogo />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/LandingScreen/LandingScreen.tsx
+++ src/components/LandingScreen/LandingScreen.tsx
@@ -1,5 +1,11 @@
+import HeaderRoot from "@/components/HeaderRoot";
 import LandingHero from "@/components/LandingHero";
 
 export default function LandingScreen() {
-  return <LandingHero />;
+  return (
+    <>
+      <HeaderRoot />
+      <LandingHero />
+    </>
+  );
 }
