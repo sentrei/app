cb9445301ec3614e657479219d483555a4b7ad71
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-23 01:02:47 +0900

    perf: ⚡️ ini resolved theme

--- src/components/DarkModeSwitch/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch/DarkModeSwitch.tsx
@@ -9,29 +9,33 @@ export default function DarkModeSwitch(): JSX.Element {
   };
 
   return (
-    <svg
-      className={clsx(
-        "w-10 h-10 cursor-pointer",
-        resolvedTheme === "dark" && "text-gray-300",
-        resolvedTheme === "light" && "text-gray-700"
+    <>
+      {resolvedTheme !== undefined && (
+        <svg
+          className={clsx(
+            "w-10 h-10 cursor-pointer text-gray-500",
+            resolvedTheme === "dark" && "text-gray-300",
+            resolvedTheme === "light" && "text-gray-700"
+          )}
+          fill={resolvedTheme === "dark" ? "none" : "currentColor"}
+          stroke={resolvedTheme === "dark" ? "currentColor" : "none"}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={toggle}
+        >
+          {resolvedTheme === "dark" && (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          )}
+          {resolvedTheme === "light" && (
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+          )}
+        </svg>
       )}
-      fill={resolvedTheme === "dark" ? "none" : undefined}
-      stroke={resolvedTheme === "dark" ? "currentColor" : undefined}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      onClick={toggle}
-    >
-      {resolvedTheme === "dark" && (
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-        />
-      )}
-      {resolvedTheme === "light" && (
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-      )}
-    </svg>
+    </>
   );
 }
