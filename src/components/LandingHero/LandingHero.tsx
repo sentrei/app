7b58327f06dcbf78d4739aabbc043133d90fc92c
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-03-24 09:47:50 -0700

    Update LandingHero.tsx

--- src/components/LandingHero/LandingHero.tsx
+++ src/components/LandingHero/LandingHero.tsx
@@ -68,18 +68,18 @@ export default function LandingHero(): JSX.Element {
       <div className="relative px-4 mx-auto mt-10 sm:px-6">
         <div className="sm:text-left md:text-center">
           <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 dark:text-gray-200 sm:text-5xl sm:leading-none md:text-6xl">
-            The all-in-one serverless <br className="hidden sm:block" /> backend
-            infrastructure <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-              of your dreams
+              Build apps fast.
             </span>
+            Jamstack backend <br className="hidden sm:block" /> on
+            the edge. <br /> 
           </h2>
           <p className="mt-3 text-base text-gray-500 sm:mt-6 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-8 md:text-xl md:max-w-3xl lg:max-w-4xl dark:text-gray-300">
-            Sentrei combines the{" "}
+            Sentrei provides the{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-              cutting-edge
+              edge serverless backend
             </span>{" "}
-            serverless technology with a global database infrastructure.
+            on top of the global database infrastructure.
             <br className="hidden lg:block" /> We abstract away the boilerplate
             to provide the best{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
